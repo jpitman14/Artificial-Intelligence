b3004ad82beef997b50b3c2163dac9d61e1f4651
jpitman14 <jack.pitman22@bathspa.ac.uk>
2025-01-23 23:24:25 +0000

    added encoding and train/test split

--- Assignment-2/Model-2/Model-2.py
+++ Assignment-2/Model-2/Model-2.py
@@ -9,3 +9,13 @@ import matplotlib.pyplot as plt
 # Loads dataset
 data = pd.read_csv("Assignment-2\\Data\\energy_data.csv")
 
+# Encoding categorical variables
+label_encoder = LabelEncoder()
+data['Day of Week'] = label_encoder.fit_transform(data['Day of Week'])
+
+# Define features and target variable
+X = data[['Square Footage', 'Number of Occupants', 'Appliances Used', 'Average Temperature', 'Day of Week']]
+y = data['Energy Consumption']
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
