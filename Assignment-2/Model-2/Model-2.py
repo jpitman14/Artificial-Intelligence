84bd7cafce52ed72223d29ddb2e46581ef3cc937
jpitman14 <jack.pitman22@bathspa.ac.uk>
2025-01-23 23:25:06 +0000

    added random forest scatter graph

--- Assignment-2/Model-2/Model-2.py
+++ Assignment-2/Model-2/Model-2.py
@@ -19,3 +19,18 @@ y = data['Energy Consumption']
 
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Random Forest Regressor Model
+random_forest_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
+random_forest_regressor.fit(X_train, y_train)
+
+# Predictions
+y_pred = random_forest_regressor.predict(X_test)
+
+# Plotting Actual vs Predicted values
+plt.scatter(y_test, y_pred)
+plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', linestyle='--')
+plt.xlabel("Actual Energy Consumption")
+plt.ylabel("Predicted Energy Consumption")
+plt.title("Random Forest Regressor: Actual vs Predicted Energy Consumption")
+plt.show()
