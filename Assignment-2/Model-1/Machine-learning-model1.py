2abb30179dd6551a0ddaf4b73c53e275a2ee6a72
jpitman14 <jack.pitman22@bathspa.ac.uk>
2025-01-23 23:19:33 +0000

    added encoding to linear regression file

--- Assignment-2/Model-1/Machine-learning-model1.py
+++ Assignment-2/Model-1/Machine-learning-model1.py
@@ -8,3 +8,11 @@ import matplotlib.pyplot as plt
 
 # Load dataset
 data = pd.read_csv("Assignment-2\\Data\\energy_data.csv")
+
+# Encoding categorical variables
+label_encoder = LabelEncoder()
+data['Day of Week'] = label_encoder.fit_transform(data['Day of Week'])
+
+# Define features and target variable
+X = data[['Square Footage', 'Number of Occupants', 'Appliances Used', 'Average Temperature', 'Day of Week']]
+y = data['Energy Consumption']
